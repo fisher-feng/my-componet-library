535bf2425479388dabed3120712545a31a213c15
fisherfeng <996337942@qq.com>
2022-09-07 00:01:23 +0800

    feat:给基本menu添加测试用例

--- src/components/Menu/menuItem.tsx
+++ src/components/Menu/menuItem.tsx
@@ -1,7 +1,7 @@
 import React, { useContext } from "react";
 import classNames from "classnames";
 import {MenuContext} from './menu'
-interface MenuProps{
+export interface MenuItemProps{
  index:number;
  disabled?:boolean;
  className?:string;
@@ -9,15 +9,15 @@ interface MenuProps{
  children?:React.ReactNode;
 }
 
-const MenuItem:React.FC<MenuProps> = (props) => {
+const MenuItem:React.FC<MenuItemProps> = (props) => {
   const {index, disabled, className, style, children} = props;
   const context = useContext(MenuContext);
   const classes = classNames(
     'menu-item',
     className,
     {
-      'is-disabled':disabled === true,
-      'is-active':context.index === index,
+      'is-disabled': disabled,
+      'is-active': context.index === index
     }
   )
   const handleClick = () => {
